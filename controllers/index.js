bacd84053dcbb8f9c8672b67feb2c0ca58888007
unknown <lutormig@gmail.com>
2022-09-05 18:24:05 -0400

    Add route _getTiposImpuestos

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 import { generateElectronicDocument } from "../services.js";
-import { getTiposTransacciones } from "../database/index.js";
+import { getTiposImpuestos, getTiposTransacciones } from "../database/index.js";
 
 export const generateDe = async (req, res) => {
   let data = req.body;
@@ -35,9 +35,29 @@ export const _getTiposTransacciones = async (_, res) => {
     return res.status(500).send({
       success: false,
       message: "",
+      data: [],
+      error: err,
+    });
+  }
+};
+export const _getTiposImpuestos = async (_, res) => {
+  try {
+    let data = await getTiposImpuestos();
+    return res.status(200).send({
+      success: true,
+      message: "",
+      data,
+      errors: [],
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send({
+      success: false,
+      message: "",
+      data: [],
       error: err,
     });
   }
 };
 
-export default { generateDe, _getTiposTransacciones };
+export default { generateDe, _getTiposTransacciones, _getTiposImpuestos };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { generateDe, _getTiposTransacciones } from "../controllers/index.js";
+import {
+  generateDe,
+  _getTiposImpuestos,
+  _getTiposTransacciones,
+} from "../controllers/index.js";
 
 export const router = express.Router();
 
 router.route("/de/event/generate").post(generateDe);
 router.route("/de/tipostransacciones").get(_getTiposTransacciones);
+router.route("/de/tiposimpuestos").get(_getTiposImpuestos);
 
 export default router;
